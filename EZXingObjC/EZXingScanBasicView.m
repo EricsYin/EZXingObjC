00c0509529d73e99ffdebb6cb36459f4f946000b
EricsYin <ericsyin7@gmail.com>
2018-07-27 00:09:06 +0800

    更新

---
+++ EZXingObjC/EZXingScanBasicView.m
@@ -0,0 +1,21 @@
+//
+//  EZXingScanBasicView.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/7/27.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EZXingScanBasicView.h"
+
+@implementation EZXingScanBasicView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
