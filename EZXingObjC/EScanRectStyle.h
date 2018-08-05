4d154ebc7f734788f8c6c6d932ceb4cb88ba3e38
EricsYin <ericsyin7@gmail.com>
2018-08-05 23:44:14 +0800

    更新样式文件

---
+++ EZXingObjC/EScanRectStyle.h
@@ -0,0 +1,13 @@
+//
+//  EScanRectStyle.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/5.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EScanRectStyle : NSObject
+
+@end
---
+++ EZXingObjC/EScanRectStyle.m
@@ -0,0 +1,13 @@
+//
+//  EScanRectStyle.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/5.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EScanRectStyle.h"
+
+@implementation EScanRectStyle
+
+@end
