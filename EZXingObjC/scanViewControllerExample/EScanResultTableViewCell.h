f22978f5a10f2046aff846e7fe1ed24924a434ad
EricsYin <ericsyin7@gmail.com>
2018-08-23 20:59:48 +0800

    更新cell

---
+++ EZXingObjC/scanViewControllerExample/EScanResultTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EScanResultTableViewCell.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/23.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EScanResultTableViewCell : UITableViewCell
+
+@end
