eb7718b43c33bef3dc49483e2fd4dd44ed24f078
EricsYin <ericsyin7@gmail.com>
2018-08-26 21:53:16 +0800

    连续扫描用例

---
+++ EZXingObjC/scanViewControllerExample/EScanRepeatViewController.h
@@ -0,0 +1,13 @@
+//
+//  EScanRepeatViewController.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/26.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EScanRepeatViewController : UIViewController
+
+@end
---
+++ EZXingObjC/scanViewControllerExample/EScanRepeatViewController.m
@@ -0,0 +1,37 @@
+//
+//  EScanRepeatViewController.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/26.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EScanRepeatViewController.h"
+
+@interface EScanRepeatViewController ()
+
+@end
+
+@implementation EScanRepeatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
