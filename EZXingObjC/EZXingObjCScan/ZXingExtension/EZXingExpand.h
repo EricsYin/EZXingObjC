33bb4b980294e8996b67686c90cb91267e319ea2
EricsYin <ericsyin7@gmail.com>
2018-08-28 21:31:24 +0800

    提交拓展初始文件

---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EZXingExpand.h
@@ -0,0 +1,13 @@
+//
+//  EZXingExpand.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/28.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EZXingExpand : NSObject
+
+@end
---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EZXingExpand.m
@@ -0,0 +1,13 @@
+//
+//  EZXingExpand.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/28.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EZXingExpand.h"
+
+@implementation EZXingExpand
+
+@end
