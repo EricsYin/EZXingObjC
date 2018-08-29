8f0df9e98f7e3fc2b45e9f7227863c188ad5e882
EricsYin <ericsyin7@gmail.com>
2018-08-29 21:06:10 +0800

    更新detext文件

---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EZXingObjectDetect.h
@@ -0,0 +1,13 @@
+//
+//  EZXingObjectDetect.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/29.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EZXingObjectDetect : NSObject
+
+@end
---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EZXingObjectDetect.m
@@ -0,0 +1,13 @@
+//
+//  EZXingObjectDetect.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/8/29.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EZXingObjectDetect.h"
+
+@implementation EZXingObjectDetect
+
+@end
