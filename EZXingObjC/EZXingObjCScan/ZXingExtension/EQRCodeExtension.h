f64cbe554b0009e19f36d8b126b2a9ec6ad07aba
EricsYin <ericsyin7@gmail.com>
2018-09-04 21:21:45 +0800

    更新QRCode文件

---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EQRCodeExtension.h
@@ -0,0 +1,13 @@
+//
+//  EQRCodeExtension.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/9/4.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EQRCodeExtension : NSObject
+
+@end
---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EQRCodeExtension.m
@@ -0,0 +1,13 @@
+//
+//  EQRCodeExtension.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/9/4.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EQRCodeExtension.h"
+
+@implementation EQRCodeExtension
+
+@end
