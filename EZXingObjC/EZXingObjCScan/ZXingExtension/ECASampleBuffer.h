b73614cf73ded61be2250d7782f3398e6e75d3b4
EricsYin <ericsyin7@gmail.com>
2018-09-22 22:24:57 +0800

    更新sample

---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/ECASampleBuffer.h
@@ -0,0 +1,17 @@
+//
+//  ECASampleBuffer.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/9/22.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ECASampleBuffer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/ECASampleBuffer.m
@@ -0,0 +1,13 @@
+//
+//  ECASampleBuffer.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/9/22.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "ECASampleBuffer.h"
+
+@implementation ECASampleBuffer
+
+@end
