c16724b0eee8b71a9964587f38a1d315adfcc53c
EricsYin <ericsyin7@gmail.com>
2018-09-27 20:30:53 +0800

    更新

---
+++ EZXingObjC/EZXingObjCScan/EAVCapture.h
@@ -0,0 +1,17 @@
+//
+//  EAVCapture.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/9/27.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EAVCapture : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EZXingObjC/EZXingObjCScan/EAVCapture.m
@@ -0,0 +1,13 @@
+//
+//  EAVCapture.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/9/27.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EAVCapture.h"
+
+@implementation EAVCapture
+
+@end
