def86c68a4ef2b022ebd59d8fc7a8e376727e072
EricsYin <ericsyin7@gmail.com>
2018-10-07 22:03:11 +0800

    OCR

---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EOCRCapture.h
@@ -0,0 +1,17 @@
+//
+//  EOCRCapture.h
+//  EZXingObjC
+//
+//  Created by erics on 2018/10/7.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EOCRCapture : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EZXingObjC/EZXingObjCScan/ZXingExtension/EOCRCapture.m
@@ -0,0 +1,13 @@
+//
+//  EOCRCapture.m
+//  EZXingObjC
+//
+//  Created by erics on 2018/10/7.
+//  Copyright © 2018年 EricsYinGroup. All rights reserved.
+//
+
+#import "EOCRCapture.h"
+
+@implementation EOCRCapture
+
+@end
